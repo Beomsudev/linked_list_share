c25461158973ee8247e99159fdfe7b5a7ed3f89e
bemosudev <bumsu1307@naver.com>
2023-07-06 18:13:10 +0900

    stl add

--- STL/stl_list_sample.cpp
+++ STL/stl_list_sample.cpp
@@ -1,37 +1,125 @@
-#include<iostream>
-#include<list>
+#include <iostream>
+#include <list>
 
 using namespace std;
 
-int main() {
 
-	//리스트 선언
-	list<int> b;
-	//리스트 포인터 선언
-	list<int>::iterator iter = b.begin();
+bool predicate(int n){
+	return n <= 2;
 
-	//뒤쪽으로 요소 추가하기
-	for (int i = 0; i < 5; i++) {
-		b.push_back(i);
-	}
+}
 
-	//양 끝의 요소 제거하기
-	b.pop_back();
-	b.pop_front();
+int main(){
+	
+	// List 생성
+	list<int> list_int {1, 2, 3, 4, 5};
+	cout << "List 생성 \n";
+	for(const auto& i: list_int) cout << i << " ";
+	cout << endl;
+	// result : 1 2 3 4 5
 
-	//중간에 요소 추가하기 (삭제는 함수만 erase로 바꿈)
-	iter = b.begin();
-	int k = 2; //두번째 자리에 8 추가하기
-	for (int i = 0; i < k - 1; ++i) {
-		++iter;
-	}
+	// push, pop
+	list<int> a_list {1, 2, 3, 4, 5};
 
-	b.insert(iter, 8);
+	cout << endl;
+	cout << "push_front()\n";
 
-	//전체 요소 출력하기
+	a_list.push_front(0);
+	for(const auto& i: a_list) cout << i << " ";
 
-	for (iter = b.begin(); iter != b.end(); iter++) {
-		cout << *iter << " ";
-	}
-	return 0;
+	cout << endl;
+
+
+	cout << endl;
+	cout << "push_back()\n";
+
+	a_list.push_back(6);
+	for(const auto& i: a_list) cout << i << " ";
+
+	cout << endl;
+
+	cout << endl;
+	cout << "pop_front()\n";
+
+	a_list.pop_front();
+	for(const auto& i: a_list) cout << i << " ";
+
+	cout << endl;
+
+	cout << endl;
+	cout << "pop_back()\n";
+
+	a_list.pop_back();
+	for(const auto& i: a_list) cout << i << " ";
+
+	cout << endl;
+
+	// insert, erase, remove, remove_if
+
+	cout << endl;
+	cout << "insert()\n";
+
+	a_list.insert(++a_list.begin(), 0);
+	for(const auto& i: a_list) cout << i << " ";
+
+	cout << endl;
+
+	cout << endl;
+	cout << "erase()\n";
+
+	a_list.erase(++a_list.begin());
+	for(const auto& i: a_list) cout << i << " ";
+
+	cout << endl;
+
+	list<int> b_list {1, 2, 3, 4, 5, 1, 1, 1, 1, 1};
+
+	cout << endl;
+	cout << "remove()\n";
+
+	b_list.remove(1);
+	for(const auto& i: b_list) cout << i << " ";
+
+	cout << endl;	
+
+	cout << endl;
+	cout << "remove_if()\n";
+
+
+	b_list.remove_if(predicate);
+	for(const auto& i: b_list) cout << i << " ";
+
+	cout << endl;	
+
+	list<int> c_list {3, 4, 2, 8, 9, 10, 2, 1};
+
+	cout << endl;
+	cout << "sort()\n";
+
+	for(const auto& i: c_list) cout << i << " ";
+	cout << endl;	
+
+	c_list.sort();
+	for(const auto& i: c_list) cout << i << " ";
+
+	cout << endl;	
+
+	cout << endl;
+	cout << "reverse()\n";
+
+	for(const auto& i: c_list) cout << i << " ";
+	cout << endl;	
+
+	c_list.reverse();
+	for(const auto& i: c_list) cout << i << " ";
+
+	cout << endl;	
+
+	cout << endl;
+	cout << "size()\n";
+
+	cout << c_list.size();
+
+	cout << endl;	
+
 }
